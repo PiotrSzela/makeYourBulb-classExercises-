6eda8b79bf92e1a8b7faef2e69d4c9a625e79240
Pan Prezes <Pan Prezes@PREZES>
2022-01-26 10:27:26 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makeYourBulb_classExercises_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bulb bulb = new Bulb("green");
+            Bulb bulb2 = new Bulb("red");
+            Bulb bulb3 = new Bulb(bulb);
+            Console.WriteLine(bulb3.BulbColor);
+
+            bulb3.BulbColor = "pink";
+
+            Console.WriteLine(bulb.BulbColor);
+            Console.WriteLine(bulb.BulbPower);
+            Console.WriteLine(bulb2.BulbColor);
+            Console.WriteLine(bulb3.BulbColor);
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Bulb
+    {
+        public string BulbColor { get; set; }
+        public int BulbPower { get; private set; }
+
+
+        public Bulb(string bulbColor,int bulbPower)
+        {
+            this.BulbColor = bulbColor;
+            this.BulbPower = bulbPower;
+
+        }
+
+        public Bulb(string bulbColor): this(bulbColor,10)
+        {
+
+        }
+
+        public Bulb(Bulb bulb) // copy constructor 
+        {
+            this.BulbColor = bulb.BulbColor;
+            this.BulbPower = bulb.BulbPower;
+
+        }
+
+    }
+}
